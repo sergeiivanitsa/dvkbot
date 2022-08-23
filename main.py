0eb2edafedc3537118e0220b782382ee98d64d6c
Sergei Ivanitsa <sergei-ivanitsa@yandex.ru>
2022-08-23 18:54:09 +1000

    Наладил парсинг наименований и ID из xml листа по URL и добавил в словарь

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
-from xml.etree import ElementTree
-import requests
-
-url = 'https://dvkeramik.ru/yml_get/2w5s1ullhoxf'
-
-response = requests.get(url)
-
-tree = ElementTree.parse(response.content)
-root = tree.getroot()
-
-print(root)
-print(root.tag, root.attrib)
-
-#import urllib.request
-#import requests
+import xml.dom.minidom as minidom
+import urllib.request
 
 
-#def get_data(xml_url):
-#    web_file = urllib.request.urlopen(xml_url)
-#    return web_file.read()
+def get_data(xml_url):
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-Agent', 'MyApp/1.0')]
+    urllib.request.install_opener(opener)
+    web_file = urllib.request.urlopen(xml_url)
+    return web_file.read()
 
 
-#if __name__ == '__main__':
-#    url = 'https://dvkeramik.ru/yml_get/2w5s1ullhoxf'
-#    response = requests.get(url)
-#    data = get_data(response)
+def get_currencies_dictionary(xml_content):
+
+    dom = minidom.parseString(xml_content)
+    dom.normalize()
+
+    elements = dom.getElementsByTagName("offer")
+    currency_dict = {}
+
+    for node in elements:
+        for child in node.childNodes:
+            if child.nodeType == 1:
+                if child.tagName == 'name':
+                    value = child.firstChild.data
+                    id = node.getAttribute("id")
+        currency_dict[value] = id
+    return currency_dict
+
+
+def print_dict(dict):
+    for key in dict.keys():
+        print(key, dict[key])
+
+
+if __name__ == '__main__':
+    url = 'https://dvkeramik.ru/yml_get/2w5s1ullhoxf'
+    currency_dict = get_currencies_dictionary(get_data(url))
+    print_dict(currency_dict)
