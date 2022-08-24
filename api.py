73d3f7c0bfad111ff6c215ae7bd92f8e00e1a175
Сергей Иваница <sergei-ivanitsa@yandex.ru>
2022-08-25 00:34:04 +1000

    четвертый

--- api.py
+++ api.py
@@ -3,23 +3,49 @@ import requests
 PASSWORD = 'S7w973oiJ8TNbb1gp403V27gsUr61jT8'
 USER = 'andreykutenkikh@gmail.com'
 
+token='5795693615:AAHZGhvouRTcQh_JmC1SWdNBuh5y7QuyXrg'
+chat_id = 279293973
+
+URL = 'http://dvkeramik.ru/api/products?q='
+
 
 session = requests.Session()
 session.auth = (USER, PASSWORD)
 
-def make_url(url):
+def make_url(URL):
     """Создает URL запрос"""
     name = input().replace(' ', '+')
-    new_url = str(url + name)
+    new_url = str(URL + name)
     return new_url
 
 
 def take_availability(endpoint):
     """Отправляет запрос на сервер и возвращает товары и их доступное для заказа количество"""
-    response = session.get(endpoint)
-    return response.content["products"]
+    dict_amount = {}
+    response = session.get(endpoint).json()
+#    name_product = response["products"][0]["product"]
+#    amount = response["products"][0]["amount"]
+#    dict_amount = {}
+#    dict_amount[name_product] = amount
+#    return dict_amount
+    for product in response["products"]:
+        name_product = product["product"]
+        amount = product["amount"]
+        dict_amount[name_product] = amount
+    return dict_amount
 
 
-if __name__ == '__main__':
-    url = 'http://dvkeramik.ru/api/products?q='
-    print(take_availability(make_url(url)))
+def print_dict(dict):
+    for key in dict.keys():
+        print(f'{key} — {dict[key]} шт.')
+
+
+#if __name__ == '__main__':
+#    url = 'http://dvkeramik.ru/api/products?q='
+#    currency_dict = take_availability(make_url(url))
+#    print_dict(currency_dict)
+
+
+def new_weather(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat.id, print_dict(take_availability(make_url(URL))))
--- main.py
+++ main.py
@@ -1,39 +1,25 @@
-import xml.dom.minidom as minidom
-import urllib.request
+import requests
+#from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+#from utility import SMILE
+import api
 
+updater = Updater(token='5795693615:AAHZGhvouRTcQh_JmC1SWdNBuh5y7QuyXrg')
+chat_id = 279293973
 
-def get_data(xml_url):
-    opener = urllib.request.build_opener()
-    opener.addheaders = [('User-Agent', 'MyApp/1.0')]
-    urllib.request.install_opener(opener)
-    web_file = urllib.request.urlopen(xml_url)
-    return web_file.read()
+def wake_up(update, context):
+    chat = update.effective_chat
+    name = update.message.chat.first_name
+#   button = ReplyKeyboardMarkup([['/Amguema'],['/Egvekinot']], resize_keyboard=True)
 
+    context.bot.send_message(
+        chat_id=chat.id,
+        text=('Привет, введи название или артикул плитки, которую хочешь найти').format(name),
+        reply_markup=button
+    )
 
-def get_currencies_dictionary(xml_content):
+updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+updater.dispatcher.add_handler(CommandHandler('Amguema', api.new_search))
 
-    dom = minidom.parseString(xml_content)
-    dom.normalize()
-
-    elements = dom.getElementsByTagName("offer")
-    currency_dict = {}
-
-    for node in elements:
-        for child in node.childNodes:
-            if child.nodeType == 1:
-                if child.tagName == 'name':
-                    value = child.firstChild.data
-                    id = node.getAttribute("id")
-        currency_dict[value] = id
-    return currency_dict
-
-
-def print_dict(dict):
-    for key in dict.keys():
-        print(key, dict[key])
-
-
-if __name__ == '__main__':
-    url = 'https://dvkeramik.ru/yml_get/2w5s1ullhoxf'
-    currency_dict = get_currencies_dictionary(get_data(url))
-    print_dict(currency_dict)
+updater.start_polling()
+updater.idle()
