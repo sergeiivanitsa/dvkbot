c33602bb6a47b81fdd2de00a985b7d724d196ef4
Sergei Ivanitsa <sergei-ivanitsa@yandex.ru>
2022-08-25 18:57:04 +1000

    Реализован полный функционал

--- api.py
+++ api.py
@@ -3,31 +3,27 @@ import requests
 PASSWORD = 'S7w973oiJ8TNbb1gp403V27gsUr61jT8'
 USER = 'andreykutenkikh@gmail.com'
 
+URL = 'http://dvkeramik.ru/api/products?q='
+PREFIX = '&page=1&items_per_page=30'
+
 token='5795693615:AAHZGhvouRTcQh_JmC1SWdNBuh5y7QuyXrg'
 chat_id = 279293973
-
-URL = 'http://dvkeramik.ru/api/products?q='
-
 
 session = requests.Session()
 session.auth = (USER, PASSWORD)
 
-def make_url(URL):
+
+def make_url(search_phrase):
     """Создает URL запрос"""
-    name = input().replace(' ', '+')
-    new_url = str(URL + name)
+    new_url = URL + search_phrase + PREFIX
     return new_url
 
 
 def take_availability(endpoint):
-    """Отправляет запрос на сервер и возвращает товары и их доступное для заказа количество"""
+    """Отправляет запрос на сервер и возвращает товары и их доступное для
+       заказа количество. Записывает полученные значения в словарь."""
     dict_amount = {}
     response = session.get(endpoint).json()
-#    name_product = response["products"][0]["product"]
-#    amount = response["products"][0]["amount"]
-#    dict_amount = {}
-#    dict_amount[name_product] = amount
-#    return dict_amount
     for product in response["products"]:
         name_product = product["product"]
         amount = product["amount"]
@@ -35,17 +31,6 @@ def take_availability(endpoint):
     return dict_amount
 
 
-def print_dict(dict):
-    for key in dict.keys():
-        print(f'{key} — {dict[key]} шт.')
-
-
-#if __name__ == '__main__':
-#    url = 'http://dvkeramik.ru/api/products?q='
-#    currency_dict = take_availability(make_url(url))
-#    print_dict(currency_dict)
-
-
-def new_weather(update, context):
-    chat = update.effective_chat
-    context.bot.send_message(chat.id, print_dict(take_availability(make_url(URL))))
+def new_search(argument):
+    answer = take_availability(make_url(argument))
+    return answer
--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-import requests
-#from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
-#from utility import SMILE
 import api
 
 updater = Updater(token='5795693615:AAHZGhvouRTcQh_JmC1SWdNBuh5y7QuyXrg')
 chat_id = 279293973
 
+
 def wake_up(update, context):
     chat = update.effective_chat
     name = update.message.chat.first_name
-#   button = ReplyKeyboardMarkup([['/Amguema'],['/Egvekinot']], resize_keyboard=True)
-
+    button = ReplyKeyboardMarkup([['/Start']], resize_keyboard=True)
     context.bot.send_message(
         chat_id=chat.id,
-        text=('Привет, введи название или артикул плитки, которую хочешь найти').format(name),
+        text=('Привет, введи название или артикул плитки, которую хочешь'
+             'найти').format(name),
         reply_markup=button
     )
 
-updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-updater.dispatcher.add_handler(CommandHandler('Amguema', api.new_search))
 
+def request_base(update, context):
+    format_request = "".join(update.message.text).replace(' ', '+')
+    dict_responce = api.new_search(format_request)
+    for product in dict_responce.keys():
+        context.bot.send_message(chat_id=update.effective_chat.id, 
+                                text=f'{product} — {dict_responce[product]} шт.')
+
+
+updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+updater.dispatcher.add_handler(MessageHandler(Filters.text &
+                              (~Filters.command), request_base))
 updater.start_polling()
 updater.idle()
